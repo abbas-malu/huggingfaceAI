1f484261972c033aff65b3baf05ddc14b5473c8b
Abbas Malu <60291537+abbas-malu@users.noreply.github.com>
2023-06-22 18:37:30 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from dotenv import find_dotenv,load_dotenv
+from transformers import pipeline
+
+load_dotenv(find_dotenv())
+
+def image2text(url):
+    image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+    text = image_to_text(url)[0]['generated_text']
+
+    print(text)
+    return text
+
+image2text('abbas.jpg')
